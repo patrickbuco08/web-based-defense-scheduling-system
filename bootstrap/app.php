6deaefdbc8261da48243f4577b7219bf4014f6c5
John Patrick Buco <patrickbuco11@gmail.com>
2025-09-16 16:46:16 +0800

    fix: tell laravel your app folder is Bocum instead of app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Bocum\BocumApplication;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return BocumApplication::configure(basePath: dirname(__DIR__))
+$app = BocumApplication::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,3 +16,9 @@ return BocumApplication::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// 👇 Tell Laravel your app folder is "Bocum" instead of "app"
+$app->useAppPath($app->basePath('Bocum'));
+
+return $app;
+
